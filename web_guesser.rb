cac9b13bd64cd4f91ec3cb3b95f406ee6ab8a071
mimilettd <mimilettd@gmail.com>
2017-08-06 23:00:17 -0600

    recover git stash

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,9 +3,14 @@ require 'sinatra/reloader'
 
 class WebGuesser
   @@secret_number = rand(100)
+  @@guess_counter = 5
 
   def secret_number
     @@secret_number
+  end
+
+  def guess_counter
+    @@guess_counter
   end
 
   def check_guess(guess)
@@ -37,6 +42,12 @@ class WebGuesser
     color
   end
 
+  def countdown
+    until @@guess_counter == 0
+      @guess_counter - 1
+    end
+  end
+
 end
 
 wg = WebGuesser.new
@@ -45,6 +56,7 @@ get '/' do
   if guess
     message = wg.check_guess(guess)
     color = wg.match_color_with_message(message)
+    wg.countdown
   end
   erb :index, :locals => {:message => message, :color => color}
 end
